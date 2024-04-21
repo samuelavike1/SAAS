093fcb8dfa4b52e5ce9cf012be8668ee9d4e2ad3
samuelavike1 <samuelavike@gmail.com>
2024-04-21 11:14:52 +0000

    migrations and models

---
+++ app/Models/UsedFeature.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UsedFeature extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'feature_id',
+        'user_id',
+        'credits',
+    ];
+}
